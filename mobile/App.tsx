ef02a8756b834bc1699413afcc8f005c982c2840
flaviosenne <flaviosenne123@gmail.com>
2022-09-13 18:17:28 -0300

    remover arquivos desnecessário do frontend, criar interface de splash e landing page do aplicativo

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,48 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StatusBar } from 'react-native';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+} from '@expo-google-fonts/inter'
+import { Background } from './src/components/Background';
+import { Home } from './src/screens/Home';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  })
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello word!!</Text>
-      <Button title="Send 1"/>
-      <Button title="Send 2"/>
-      <Button title="Send 3"/>
-      <Button title="Send 4"/>
-      <Button title="Send 5"/>
-      <StatusBar style="auto" backgroundColor='yellow'/>
-    </View>
+    <Background>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Home/> : <Loading/>}
+    </Background>
   );
 }
-
-interface ButtonProps {
-  title: string
-}
-
-function Button(props: ButtonProps){
-  return (
-    <TouchableOpacity>
-      <Text style={styles.button}>
-        {props.title}
-      </Text>
-    </TouchableOpacity>
-  )
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    color: '#fff',
-    fontSize: 22
-  },
-  button: {
-    color: '#fff',
-    borderStyle: 'dashed',
-    borderColor: '#fff'
-  }
-});
